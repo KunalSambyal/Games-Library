0936647d74be66f77469168007309ca5ac43497f
Kunal Sambyal <kunalsambyal9628@gmail.com>
2026-06-19 16:22:52 +0530

    for fetching games data

---
+++ src/services/api.ts
@@ -0,0 +1,13 @@
+export async function getGamesData() {
+    try {
+        const response = await fetch("http://localhost:5173/games.json");
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch games: ${response.statusText}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error("Error fetching games data:", error);
+    }
+}
