d335efa23357b6db75d3bdfa06f9b2416b2a1925
Kunal Sambyal <kunalsambyal9628@gmail.com>
2026-06-19 18:12:38 +0530

    feature: implement responsive hamburger menu

--- src/components/HamMenu.tsx
+++ src/components/HamMenu.tsx
@@ -1,14 +1,21 @@
 import type { ReactElement } from "react";
 
-function HamMenu() {
-    let isActive = true;
-    const hamMenuIcon: ReactElement = isActive ? (
+interface HamMenuProps {
+    isOpen: boolean;
+    onClick: () => void;
+}
+
+function HamMenu({ isOpen, onClick }: HamMenuProps) {
+    const hamMenuIcon: ReactElement = isOpen ? (
         <i className="fa-solid fa-xmark"></i>
     ) : (
         <i className="fa-solid fa-bars"></i>
     );
     return (
-        <button className="dark:hover:bg-neutral-700 hover:bg-neutral-200 duration-200 rounded-full p-1 cursor-pointer text-xl md:hidden block">
+        <button
+            onClick={onClick}
+            className="dark:hover:bg-neutral-700 hover:bg-neutral-200 duration-200 rounded-full p-1 cursor-pointer text-xl md:hidden block"
+        >
             {hamMenuIcon}
         </button>
     );
--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,12 +1,21 @@
 import { Link } from "react-router-dom";
 
-function NavLinks() {
+interface NavLinksProps {
+    className?: string;
+    onLinkClick?: () => void;
+}
+
+function NavLinks({
+    className = "flex items-center gap-x-4",
+    onLinkClick,
+}: NavLinksProps) {
     const navbarLinks: string[] = ["Home", "WishList", "Favourites"];
     return (
-        <ul className="flex items-center gap-x-4">
+        <ul className={className}>
             {navbarLinks.map((link, index) => (
                 <li key={link}>
                     <Link
+                        onClick={onLinkClick}
                         className="dark:hover:bg-neutral-600 hover:bg-neutral-200 duration-200 px-2 py-1 rounded-sm font-semibold"
                         to={index === 0 ? "/" : "/" + link.toLocaleLowerCase()}
                     >
